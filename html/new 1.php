93cbec872f98155c0f5a5cbd6b6a6504ed0c9276
mohan <mohanreddymohan2053@gmail.com>
2020-09-22 23:58:02 +0530

    Resume

---
+++ html/new 1.php
@@ -0,0 +1,14 @@
+<div class="container-fluid">
+<div class="container py-3">
+ <h1 class="text-center text-primary"> My Work</h1>
+ <div class="lead text-center">Im Working Profiles PWA And Bootstrao HTML</div>
+ <div class="row my-4 pt-4">
+  <div class="col-sm-12 col-md-6 text-center">
+   <div class="card shadow h-100">
+    <img class="card-img-top img-fluid "src="images/p1.jpeg" alt="img-profile">
+   <div class="card-footer text-center">
+    <h4 class="card-title">Work at Home</h>
+     <p class="card-text"> Online by APSSDC</p>
+   </div>
+  </div> 
+ </div>
